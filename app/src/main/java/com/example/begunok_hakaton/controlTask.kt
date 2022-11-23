592e953608273c898486459e3e37dff3a600fb0d
Zaycev MA <solynkaz@yandex.ru>
2022-11-23 17:11:41 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/begunok_hakaton/controlTask.kt
@@ -0,0 +1,11 @@
+package com.example.begunok_hakaton
+
+data class controlTask(
+    val isQuestIssued: Boolean = false,
+    val theme: String = "",
+    val maxPoint: String = "",
+    val currentPoint: String = "",
+
+) {
+
+}
